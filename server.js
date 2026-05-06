d08c4b030854981a3f19aa06a7aa2c4e4b124e49
ourskillstore033 <ourskillstore@gmail.com>
2026-05-06 20:38:00 +0530

    server.js

--- server.js
+++ server.js
@@ -63,3 +63,10 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+
+
+const path = require("path");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
